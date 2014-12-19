15be72dca228d9d98f037636dae7dd1542c6e235
John Matthew <john@compunique.com>
2014-12-19 12:28:10 -0800

    Changes for email advice

--- alerter/management/commands/advice.py
+++ alerter/management/commands/advice.py
@@ -1,70 +1,43 @@
 from django.core.management.base import BaseCommand
-from django.db.models import F
 from django.conf import settings
-
+from django.core.mail import send_mail
 from django.template.loader import render_to_string
-
 import logging
-
-from alerter.models import TradeAlert
-from aim.models import Holding
-
-import time
+from users.models import User
 
 logger = logging.getLogger(__name__)
 
-def generate_alerts():
+def do_alerts():
+    logger.info("Do alerts()")
 
-    logger.info("generate_alerts() - Start")    
+    alert_set = []
 
-    # our starting list ( needs to be optimized).  Only pick holdings that aren't already alerted for
-    # emailed or not.
-    alertlist = Holding.objects.exclude(currentalert=None).exclude(tradealert__alert=F("currentalert") )
-    
-    for h in alertlist:
-        # see if these
+    # loop over all users and see if any of them have alerts, if so, pass the whole user portfolio list to them and be done with it.
+    for u in User.objects.all():
+
+        alert_set = []          ## Alert set is all the holdings for this user that have alerts behind them.
         
-        if h.alert():
-            # this holding has an alert that hasn't been emailed before
-            logger.debug("Alert found for %s" % h)
-            ta = TradeAlert()
-            ta.holding = h
-            ta.alert = h.currentalert
-            ta.date = time.strftime("%Y-%m-%d")
-            ta.save()
-
-    logger.info("generate_alerts() - End")    
-
-
-def email_alerts():
-    logger.info("email_alerts() - Start")
-    
-    for e in TradeAlert.objects.filter(emailed=False):
-        ctx = {"alert" : e}
+        logger.debug("checking %s" % u)        
         
-        subject  = render_to_string("alerter/email_subject.txt", ctx)
-        subject = ''.join(subject.splitlines())
-
-        message = render_to_string("alerter/email.txt",ctx)
-
-        mfrom = settings.EMAIL_HOST_USER
-         
-        try:
-            e.holding.portfolio.owner.email_user(subject, message, mfrom)
-            e.emailed = True
-            e.save()
-            logger.debug("Emailed user %s" % subject)
+        for p in u.portfolio_set.all().order_by("name"):
+            logger.debug("Checking %s" % p)
             
-        except:
-            logger.exception("Error emailing alert to user") 
+            for h in p.holding_set.all().order_by("symbol__name"):
 
-    logger.info("email_alerts() - End")
-                                             
+                logger.debug("Checking %s" % h)
+                
+                if h.alert():
+                    alert_set.append(h)
+
+        if alert_set:
+            ctx = {"alert_set" : alert_set }
+            message = render_to_string("alerter/email_report.html",ctx)
+            mfrom = settings.EMAIL_HOST_USER
+            send_mail(subject="Subject", message=None, html_message=message, from_email=mfrom, recipient_list=[u.email,])
 
 class Command(BaseCommand):
     args = None
     help = "Generates and emails all stock alerts"
     
     def handle(self, *args, **options):
-        generate_alerts()
-        email_alerts()
+        do_alerts()
